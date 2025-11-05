8918f9174ba93cc46b48287877c61f3d650ab94a
Vineetyadav2 <yvineet.2411@gmail.com>
2025-11-05 16:35:35 +0530

    Initial commit containing JQueryUI

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    jQuery.validationEngineLanguage.allRules['passwordCheck'] = {
+  "regex": /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).+$/,
+  "alertText": "Password me kam se kam ek uppercase, ek lowercase, aur ek special character hona chahiye"
+    };
+
+  $("#formDialog").dialog({
+    autoOpen: false,
+    modal: true,
+    width: 400
+  });
+
+  $("#addRecordBtn").click(function() {
+    $("#formDialog").dialog("open");
+  });
+
+  $("#recordForm").validationEngine();
+
+  $("#recordForm").submit(function(event) {
+    event.preventDefault();
+
+    
+    // if (!$(this).validationEngine('validate')) {
+    //   return;
+    // }
+
+    const name = $("#name").val();
+    const email = $("#email").val();
+    const age = $("#age").val();
+
+    const newRow = `
+      <tr>
+        <td>${name}</td>
+        <td>${email}</td>
+        <td>${age}</td>
+      </tr>
+    `;
+
+    $("#dataTable tbody").append(newRow);
+
+    $("#recordForm")[0].reset();
+    $("#formDialog").dialog("close");
+  });
+});
+
+
